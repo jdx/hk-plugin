71b01897382919af933724a0b2d19502df473b7a
dickeyxxx <jeff@dickeyxxx.com>
2014-10-23 01:01:20 -0700

    tweak

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ console.log('loaded!')
 module.exports = function () {
   console.log('i am in the exported function')
 }
+module.exports.help = function () {
+  console.log('1. here is some useful help')
+  console.log('2. here is some useful help')
+  console.log('3. here is some useful help')
+  console.log('4. here is some useful help')
+}
